d1ba29a4fe93cb80accec44e9ba48af7cc6ff1b0
Ali Barış Öztürk <haan78@gmail.com>
2021-09-10 00:42:35 +0300

    Empty string to nulll

--- src/lib/Subutai.js
+++ src/lib/Subutai.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default {
+    useDataTransform:true,
     ajaxActivationCount: 0,
     COOKIE_NAME : "SUBUTAI",
     COOKIE_DATA : false,
@@ -79,12 +80,29 @@ export default {
         }
     },
 
+    dataTransform(data) {
+        if ( typeof data === "object" && data !== null ) {
+            for( var k in data ) {
+                data[k] = this.dataTransform( data[k] );
+            }
+        } else if (typeof data === "string" ) {
+            var str = data.trim();
+            if ( str === "" ) {
+                return null;
+            } else {
+                return str;
+            }
+        }
+        return data;
+    },
+
     ajax(url, data, onSuccess, onError) {
         let self = this;
         let config = {};
         var err = (typeof onError === "function" ? onError : self.ajaxDefaultError);
         self.up();
-        axios.post(url, (data ? data : null), config).then((response) => {            
+        var d = ( typeof data !== "undefined" ? this.dataTransform(data) : null );
+        axios.post(url, d, config).then((response) => {            
             if (typeof response.data === "object" && typeof response.data.success === "boolean") {
                 if (response.data.success) {
                     if (typeof onSuccess === "function") {
