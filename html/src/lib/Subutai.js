bd06f8899d47411704ea8fb7b20ddbc82a096d2e
Ali Barış Öztürk <alibarisozturk@gmail.com>
2021-10-01 12:31:53 +0300

    subutai

--- html/src/lib/Subutai.js
+++
@@ -1,8 +0,0 @@
-import http from "./SubutaiHttp";
-import cookie from "./SubutaiCookie";
-
-
-export default {
-    "cookie":cookie,
-    "http":http
-}
--- html/src/lib/SubutaiVue.js
+++ html/src/lib/SubutaiVue.js
@@ -1,7 +1,12 @@
-import Subutai from "./Subutai";
+import http from "./SubutaiHttp";
+import cookie from "./SubutaiCookie";
+
 export default {
     install: (app) => {
         // Plugin code goes here
-        app.config.globalProperties.$subutai = Subutai;
+        app.config.globalProperties.$subutai = {
+            "cookie":cookie,
+            "http":http
+        };
     }
 }
