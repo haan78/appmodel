05a36ea93145093df3bb8cff0939c9e761920985
Ali Barış Öztürk <alibarisozturk@gmail.com>
2021-10-07 15:20:32 +0300

    junk

--- html/src/lib/junk.js
+++ html/src/lib/junk.js
@@ -20,6 +20,16 @@ export default {
             }
         }
     },
+
+    __TurkishUpper() {
+        String.prototype.turkishToUpper = function(){
+            var string = this;
+            var letters = { "i": "İ", "ş": "Ş", "ğ": "Ğ", "ü": "Ü", "ö": "Ö", "ç": "Ç", "ı": "I" };
+            string = string.replace(/(([iışğüçö]))/g, function(letter){ return letters[letter]; })
+            return string.toUpperCase();
+        };
+    },
+    
     binarySubset(arr) {
         var result = [];
         var length = arr.length;
